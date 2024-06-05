7d19d2351e9164cb1cffb914a33ff37fa920a374
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-05 16:19:53 +0530

    Decleration Merging: identifier,Namespace

---
+++ 01-Decleration-Merging.ts
@@ -0,0 +1,61 @@
+/* Identifier
+Namespaces
+Classes
+
+ */
+
+// identifier
+
+const is_a_value = 4
+type is_a_type = {}
+namespace is_a_namespace {
+  const foo = 17
+}
+ 
+// how to test for a (value | namespace)
+const x = is_a_value // the value position (RHS of =).
+
+// how to test for a type
+const y: is_a_type = {} // the type position (LHS of =).
+
+// how to test for a namespace (hover over is_a_namespace symbol)
+is_a_namespace
+
+
+
+
+// namespace
+
+// a `fetch` kind of function
+$.ajax({
+    url: "/api/getWeather",
+    data: {
+      zipcode: 97201,
+    },
+    success: function (result) {
+      $("#weather-temp")[0].innerHTML =
+        "<strong>" + result + "</strong> degrees"
+    },
+  })
+  // a `document.querySelectorAll` kind of function
+  $("h1.title").forEach((node) => {
+    node.tagName // "h1"
+           
+  })
+
+
+
+
+  function $(selector: string): NodeListOf<Element> {
+    return document.querySelectorAll(selector)
+  }
+  namespace $ {
+    export function ajax(arg: {
+      url: string
+      data: any
+      success: (response: any) => void
+    }): Promise<any> {
+      return Promise.resolve()
+    }
+  }
+  
