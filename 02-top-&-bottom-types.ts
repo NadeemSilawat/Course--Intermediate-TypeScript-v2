009d2194d972248bc757d314fa4617f413d641cf
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-07 12:57:59 +0530

    Top & Bottom Types :Object & Empty Object

--- 02-top-&-bottom-types.ts
+++ 02-top-&-bottom-types.ts
@@ -99,7 +99,7 @@ console.log(doSomethingRisky());
 
 
         /* Almost top type: object */
-
+/* 
         let val: object = { status: "ok" }
         val = "foo"
        // Type 'string' is not assignable to type 'object'.
@@ -122,3 +122,47 @@ console.log(doSomethingRisky());
               = { success: "ok", data: [] }
          
         val = response        
+         */
+
+        /* Almost top type: {} */
+// The empty object type {} represents the set { all possible values, except for null and undefined }
+
+/* 
+const stringOrNumber: string | number = 4
+let nullableString: string | null = null
+const myObj: {
+  a?: number
+  b: string
+} = { b: "foo" }
+ 
+ 
+let val2: {} = 4
+val2 = "abc"
+val2 = new Date()
+val2 = stringOrNumber
+val2 = nullableString
+// Type 'null' is not assignable to type '{}'.
+val2 = myObj.a
+/* Type 'number | undefined' is not assignable to type '{}'.
+  Type 'undefined' is not assignable to type '{}'. */
+
+
+
+
+
+
+  /* let withoutUndefined: {} | null = 37
+let withUndefined: {} | null | undefined = 38
+let anUnknown: unknown = "42"
+  */
+ 
+/* withoutUndefined = anUnknown // ❌
+// Type 'unknown' is not assignable to type '{} | null'.
+
+withUndefined = anUnknown // ✅
+ */
+
+type NullableStringOrNumber = string | number | null | undefined;
+type StringOrNumber = NullableStringOrNumber & {}
+
+
