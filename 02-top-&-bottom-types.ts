29d904edf295796197b6c2d45785c1a9136af190
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-07 12:28:05 +0530

    Top & Bottom Types : top-any

---
+++ 02-top-&-bottom-types.ts
@@ -0,0 +1,22 @@
+    // Top types
+
+/* A top type (symbol: ⊤) is a type that describes any possible value allowed by the system.  */
+
+// TypeScript provides two of these types: any and unknown.
+
+
+        /* any */
+
+let flexible: any = 4
+flexible = "Download some more ram"
+flexible = window.document
+flexible = setTimeout      
+
+
+flexible.it.is.possible.to.access.any.deep.property
+
+console.log(window, Promise, setTimeout, "foo")
+
+
+
+     
