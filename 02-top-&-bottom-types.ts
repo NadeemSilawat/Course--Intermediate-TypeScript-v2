c9c0efb2c8d664dc0d922ce35d7eed30c5765921
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-07 12:29:04 +0530

    Top & Bottom Types :Top-unknown

--- 02-top-&-bottom-types.ts
+++ 02-top-&-bottom-types.ts
@@ -19,4 +19,33 @@ console.log(window, Promise, setTimeout, "foo")
 
 
 
-     
+        /* unknown */
+// Like any, unknown can accept any value that is possible to create in JavaScript:
+
+let flexible: unknown = 4
+flexible = "Download some more ram"
+flexible = window.document
+flexible = setTimeout
+
+let myUnknown: unknown = 14
+myUnknown.it.is.possible.to.access.any.deep.property
+        // Error: Object is of type 'unknown'.
+
+
+        // This code runs for myUnknown = { all possible values }
+if (typeof myUnknown === "string") {
+    // This code runs for myUnknown = { all strings }
+          
+  let myUnknown: string
+  } else if (typeof myUnknown === "number") {
+    // This code runs for myUnknown = { all numbers }
+   
+       
+  let myUnknown: number
+  } else {
+   
+       
+  let myUnknown: unknown
+    // this would run for "the leftovers"
+    //       myUnknown = { anything except string or numbers }
+  }
