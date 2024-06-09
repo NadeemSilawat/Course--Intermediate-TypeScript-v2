36497f5e9b53214023a44927325773e03415cd0f
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-09 20:39:28 +0530

    Nullish Value: Null & Undefined

---
+++ 03-nullish-values.ts
@@ -0,0 +1,34 @@
+/* Nullish values */
+
+/* --------null----------- */
+
+/* null means: there is a value, and that value is nothing.
+While some people believe that null is not an important part of the 
+JS language, I find that it’s useful to express the concept of a “nothing”
+ result(kind of like an empty array, but not an array).
+ */
+
+ const userInfo = {
+    name: "Mike",
+    email: "mike@example.com",
+    secondaryEmail: null, // user has no secondary email
+  }
+
+
+
+/* --------undefined----------- */
+
+/* undefined means the value isn’t available (yet?) */
+
+interface FormInProgress {
+    createdAt: Date
+    data: FormData
+    completedAt?: Date
+  }
+  const formInProgress: FormInProgress = {
+    createdAt: new Date(),
+    data: new FormData(),
+  }
+  function submitForm() {
+    formInProgress.completedAt = new Date()
+  }
