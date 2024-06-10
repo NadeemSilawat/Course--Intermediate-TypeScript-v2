9b932ab50b54bc19a267b1ac38ac0653f97cb849
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-10 17:45:13 +0530

    Optional chaining ?.

--- 03-nullish-values.ts
+++ 03-nullish-values.ts
@@ -8,11 +8,11 @@ JS language, I find that it’s useful to express the concept of a “nothing”
  result(kind of like an empty array, but not an array).
  */
 
- const userInfo = {
-    name: "Mike",
-    email: "mike@example.com",
-    secondaryEmail: null, // user has no secondary email
-  }
+const userInfo = {
+  name: "Mike",
+  email: "mike@example.com",
+  secondaryEmail: null, // user has no secondary email
+}
 
 
 
@@ -21,17 +21,17 @@ JS language, I find that it’s useful to express the concept of a “nothing”
 /* undefined means the value isn’t available (yet?) */
 
 interface FormInProgress {
-    createdAt: Date
-    data: FormData
-    completedAt?: Date
-  }
-  const formInProgress: FormInProgress = {
-    createdAt: new Date(),
-    data: new FormData(),
-  }
-  function submitForm() {
-    formInProgress.completedAt = new Date()
-  }
+  createdAt: Date
+  data: FormData
+  completedAt?: Date
+}
+const formInProgress: FormInProgress = {
+  createdAt: new Date(),
+  data: new FormData(),
+}
+function submitForm() {
+  formInProgress.completedAt = new Date()
+}
 
 
 
@@ -40,9 +40,9 @@ interface FormInProgress {
 /* void should exclusively be used to describe that a
  function’s return value should be ignored */
 
- console.log(`console.log returns nothing.`)
-        
-        // TypeScript log-> : (method) Console.log(...data: any[]): void
+console.log(`console.log returns nothing.`)
+
+// TypeScript log-> : (method) Console.log(...data: any[]): void
 
 
 
@@ -77,10 +77,10 @@ about a class field being used, when it can’t be proven1 that it was initializ
 */
 
 class ThingWithAsyncSetup {
-  setupPromise: Promise<any> 
+  setupPromise: Promise<any>
   isSetup: boolean
-//  Erorr : Property 'isSetup' has no initializer and is not definitely assigned in the constructor.
- 
+  //  Erorr : Property 'isSetup' has no initializer and is not definitely assigned in the constructor.
+
   constructor() {
     this.setupPromise = new Promise((resolve) => {
       this.isSetup = false
@@ -89,7 +89,7 @@ class ThingWithAsyncSetup {
       this.isSetup = true
     })
   }
- 
+
   private async doSetup(resolve: (value: unknown) => void) {
     // some async stuff
   }
@@ -98,7 +98,63 @@ class ThingWithAsyncSetup {
 
 let myThing = new ThingWithAsyncSetup()
 myThing.isSetup // what if this isn't assigned yet?
-          
+
 // (property) ThingWithAsyncSetup.isSetup: boolean
 
-  
+
+
+/* --------Optional chaining ?.----------- */
+
+// A less hazardous tool, relative to the non-null assertion operator is optional chaining.
+
+// Let’s say we have a big JSON object with a structure like this
+
+type Payment = {
+  id: string
+  amount: number
+  createdAt: Date
+}
+type Invoice = {
+  id: string
+  due: number
+  payments: Payment[]
+  lastPayment?: Payment
+  createdAt: Date
+}
+type Customer = { 
+  id: string,
+  lastInvoice?: Invoice
+  invoices: Invoice[]
+};
+type ResponseData = {
+  customers?: Customer[]
+  customer?: Customer
+}
+
+
+function getLastPayment(data: ResponseData): number | undefined {
+  const {customer} = data;
+  if (!customer) return;
+ 
+  const { lastInvoice } = customer;
+  if (!lastInvoice) return;
+ 
+  const { lastPayment } = lastInvoice;
+  if (!lastPayment) return;
+ 
+  return lastPayment.amount;
+}
+
+/* 
+function getLastPayment(data: ResponseData): number | undefined {
+  return data?.customer?.lastInvoice?.lastPayment?.amount
+}
+ */
+
+
+/* function getLastPayment(data) {
+  var _a, _b, _c;
+  return (_c = (_b = (_a = data === null || data === void 0 ? void 0 : data.customer) === null || _a === void 0 ? void 0 : _a.lastInvoice) === null || _b === void 0 ? void 0 : _b.lastPayment) === null || _c === void 0 ? void 0 : _c.amount;
+}
+
+ */
