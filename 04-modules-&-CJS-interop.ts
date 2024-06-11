acfb9f2f113148e2833d7bf1559d8538390c2661
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-11 15:36:58 +0530

    ES Module imports and exports

---
+++ 04-modules-&-CJS-interop.ts
@@ -0,0 +1,21 @@
+
+/* ---------ES Module imports and exports------- */
+
+    // named imports
+/*
+    import { Blueberry, Raspberry } from './berries'
+    import Kiwi from './kiwi' // default import
+    export function makeFruitSalad() {} // named export
+    export default class FruitBasket {} // default export
+    export { lemon, lime } from './citrus' // re-export
+    export * as berries from './berries' // re-export entire module as a single namespace
+ */
+
+/* 
+    import * as allBerries from "./berries" // namespace import
+    allBerries.Strawberry // using the namespace
+    allBerries.Blueberry
+    allBerries.Raspberry
+    export * from "./berries" // namespace re-export
+ */
+
