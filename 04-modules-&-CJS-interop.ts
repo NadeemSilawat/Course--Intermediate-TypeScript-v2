eba0ef8b7cb373efee3bea2b8726a643db848136
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-12 15:54:55 +0530

    Native ES module support

--- 04-modules-&-CJS-interop.ts
+++ 04-modules-&-CJS-interop.ts
@@ -56,6 +56,40 @@ let x: Raspberry = { color: 'red' }; */
 /* 
 In this scenario, if we converted the index.js file to TypeScript, we could
  continue to use the banana.js module as-is by using a namespace import */
-
-
  
+
+// Common Js Example :- 
+
+    // const fs = require("fs")
+    // fs.readFileSync('example.txt')
+
+// module ES6 Example:-
+// namespace import
+    // import * as fs from "fs"
+    // fs.readFileSync('example.txt')
+
+
+
+
+
+
+
+
+    /* Native ES module support */
+
+
+/*     This means you can natively run code containing thing like import { Foo } from 'bar', use top-level await and more!
+
+    How to unambiguously indicate which type of module you’re authoring
+    
+    Files with the .mjs extension are treated as native ES modules
+    Files with the .cjs extension are treated as CJS modules
+    You can also indicate whether .js files in your project should be treated as ES or CJS modules. In your package.json you may include a top-level "type" field with either of the following values
+    
+    "module" indicates that .js files should be run as ES modules
+    "commonjs" indicates that .js files should be run as CommonJS
+    Note that even as of Node 20.8.0, Node.js still assumes .js files are CommonJS if you specify no "type" field at all in your "package.json"
+ */    
+    
+
+
